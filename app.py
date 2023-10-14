a4030d01690e4284bdbb11c2697f3c144d8c72b2
Siddhant Mishra <90679433+siddhantmishra-4098@users.noreply.github.com>
2023-10-14 06:12:25 +0530

    app.py

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ else:
 )
 
     elif class_names[np.argmax(predictions)] == 'no-tire':
-        st.sidebar.error("No Tire Detected")
-        st.sidebar.info("No tire was detected")
+        st.sidebar.error("Deflated Tire")
+        st.sidebar.info("Your vehicle's tire is deflated. Consider taking a deeper look for possible wear and tears")
         st.sidebar.markdown(
     f'<a href="https://tire-and-damage-fislpbhn9javpmtamucqpo.streamlit.app/" target="_blank" style="display: inline-block; padding: 12px 20px; background-color: black; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Tire Damage Segmentation</a>',
     unsafe_allow_html=True
